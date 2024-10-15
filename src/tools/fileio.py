ad96874dea555d1b3a86474b556f0207d23eb552
sqy <2329772287@qq.com>
2024-10-15 19:54:01 +0800

    add base structure of the project

--- src/tools/fileio.py
+++ src/tools/fileio.py
@@ -6,6 +6,7 @@ def read_txt_file(file_path):
         content = file.read()
     return content
 
+
 def write_json_file(file_path, data):
     with open(file_path, 'w') as file:
         json.dump(data, file)
