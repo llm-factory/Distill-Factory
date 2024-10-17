fae5d9206c5ec64a7e891e0a1974d43f0f810e8a
sqy <2329772287@qq.com>
2024-10-17 11:02:13 +0800

    fix type of config.temperature default value

--- src/model/config.py
+++ src/model/config.py
@@ -11,7 +11,7 @@ class Config:
         self.base_url: str = openai_config.get("base_url")
         self.api_key: str = openai_config.get("api_key")
         self.model: str = openai_config.get("model")
-        self.temperature: float = openai_config.get("temperature","1.0")
+        self.temperature: float = openai_config.get("temperature", 1.0)
         self.save_dir: str = self.conf_dict.get("save_dir")
         self.main_theme: str= self.conf_dict.get("main_theme")
         self.file_path: Path = self.conf_dict.get("file_path")
